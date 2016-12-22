e1cef0c69f22ba7443ab7c3338482de18841ed84
Edian Comachio <ediancomachio@hotmail.com>
2016-12-22 20:00:57 -0200

    mentoring section 100% not responsive yet

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,50 @@
-//$( document ).ready(function() {});
+$( document ).ready(function() {
+  mentoringBubbleClick();
+});
+
+function mentoringBubbleClick(){
+
+  if($(window).width() > 640){
+    showBubble();
+  }else {
+    showModal();
+  }
+
+}
+
+function showModal(){
+  $('.face').on('click', function(){    
+    $(this).addClass('has-bubble-open')
+      .siblings().removeClass('has-bubble-open');
+  });
+}
+
+$('.face').on('click', function(){
+  $('.faces:nth-child(3)').addClass('has-bubble-open');
+  $this = $(this);
+  var parentTop = $this.position().top
+  res = (parentTop - 230) * -1;
+  $('.faces').css('top', + res  + 'px');
+
+  $(this).addClass('has-bubble-open')
+  .siblings().removeClass('has-bubble-open');
+});
+function showBubble(){
+}
 
 $(window).scroll(function(){
   youtubeVidScroll();
+  startMentoring();
 });
+
+function startMentoring(){
+
+  var wScroll = $(window).scrollTop();
+  if($('section.mentoring').offset().top - 500 < wScroll){
+    $('.faces').addClass('launched');
+  }
+
+}
 
 function youtubeVidScroll(){
   var yScroll = $(window).scrollTop();
